14bb127ea7e368071133087b409ab7e480ddb34c
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-22 19:41:50 +0530

    Added tab view to navigate across various views

---
+++ AvocadoRecipesApp/View/AvocadosView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadosView.swift
+//  AvocadoRecipesApp
+//
+//  Created by Usha Sai Chintha on 22/09/22.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text("Avocados")
+    }
+}
+
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
