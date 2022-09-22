2623a0af585d3a28844a5c2ba940791690d10f57
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-22 14:55:37 +0530

    Initial Commit

---
+++ AvocadoRecipesApp/AvocadoRecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoRecipesAppApp.swift
+//  AvocadoRecipesApp
+//
+//  Created by Usha Sai Chintha on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoRecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
