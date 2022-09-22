cb07069603695f16ce89b4b7d34c6db255486630
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-22 20:29:36 +0530

    Added header view for avocados

--- AvocadoRecipesApp/View/ContentView.swift
+++ AvocadoRecipesApp/View/ContentView.swift
@@ -1,15 +1,36 @@
-//
-//  ContentView.swift
-//  AvocadoRecipesApp
-//
-//  Created by Usha Sai Chintha on 22/09/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            Text("Avocado recipes")
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack(alignment: .center, spacing: 20)  {
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                VStack(alignment: .center, spacing: 20){
+                    Text("All about Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about avocados but were too afraid to ask")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
